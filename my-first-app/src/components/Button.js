326a65fcc465f8e8b4298acd854b31a88d6de8d5
catalin <44466064+KravinovT@users.noreply.github.com>
2018-11-06 17:39:46 +0200

    Add button component. Cleaned up project.

---
+++ my-first-app/src/components/Button.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class Button extends Component {
+    render(){
+        return (
+            <button id="pressMe">+1</button>
+        );
+    }
+}
+
+export default Button
