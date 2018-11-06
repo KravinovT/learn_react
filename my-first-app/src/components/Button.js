d4cf30c16bbd00ff284132c64c3bdaed7d89438e
catalin <44466064+KravinovT@users.noreply.github.com>
2018-11-06 17:52:06 +0200

    Added properties to button component.

--- my-first-app/src/components/Button.js
+++ my-first-app/src/components/Button.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types'
 
 class Button extends Component {
+    
+    constructor(props){
+        super(props);
+        this.onClick=props.onClick;
+    }
+
     render(){
         return (
             <button id="pressMe">+1</button>
         );
-    }
+    }    
 }
+
+Button.propTypes = {
+    onClick: PropTypes.func.required
+};
 
 export default Button
